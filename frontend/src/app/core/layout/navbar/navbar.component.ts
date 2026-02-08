4bfaeb2ccf5436941c94dd3a1f7c51b423d28476
dinos <dinostupar78@gmail.com>
2026-02-08 18:37:56 +0100

    Creating app structure,

--- frontend/src/app/core/layout/navbar/navbar.component.ts
+++ frontend/src/app/core/layout/navbar/navbar.component.ts
@@ -5,15 +5,15 @@ import { faRightToBracket  } from '@fortawesome/free-solid-svg-icons';
 
 
 @Component({
-  selector: 'app-navbar-component',
+  selector: 'app-navbar',
   imports: [
     FaIconComponent,
     FontAwesomeModule,
 
 
   ],
-  templateUrl: './navbar-component.html',
-  styleUrl: './navbar-component.css',
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.css',
 })
 export class NavbarComponent {
   constructor(private router: Router){}
