1f6d9bf4b17f20f79d847c986680c5c234b27712
dinos <dinostupar78@gmail.com>
2026-02-08 18:31:19 +0100

    Creating app structure, Modifying navbar/footer

---
+++ frontend/src/app/core/layout/navbar-component/navbar-component.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+import {FaIconComponent, FontAwesomeModule} from '@fortawesome/angular-fontawesome';
+import {Router, RouterLink} from '@angular/router';
+import { faRightToBracket  } from '@fortawesome/free-solid-svg-icons';
+
+
+@Component({
+  selector: 'app-navbar-component',
+  imports: [
+    FaIconComponent,
+    FontAwesomeModule,
+
+
+  ],
+  templateUrl: './navbar-component.html',
+  styleUrl: './navbar-component.css',
+})
+export class NavbarComponent {
+  constructor(private router: Router){}
+
+  navbarOpened = false;
+  isHovered = false;
+
+  toggleNavbar() {
+    this.navbarOpened = !this.navbarOpened;
+  }
+
+  faRightToBracket = faRightToBracket;
+
+}
