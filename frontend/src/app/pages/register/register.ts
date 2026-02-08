1307c669c4c492041820a18c328e42951c766cbf
dinos <dinostupar78@gmail.com>
2026-02-08 21:42:24 +0100

    Creating frontend

--- frontend/src/app/pages/register/register.ts
+++ frontend/src/app/pages/register/register.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-register',
@@ -6,6 +6,11 @@ import { Component } from '@angular/core';
   templateUrl: './register.html',
   styleUrl: './register.css',
 })
-export class Register {
+export class Register implements OnInit {
+    ngOnInit(): void {
+    }
+
+  isHovered = false;
+
 
 }
