84d0ef67aa99ebc2b7d123958829902904fd0271
dinos <dinostupar78@gmail.com>
2026-02-16 22:26:43 +0100

    Loans API endpoint, overhaul

---
+++ frontend/src/app/pages/loans/loans.ts
@@ -0,0 +1,55 @@
+import {Component, OnInit} from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {LoansService} from '../../core/services/loans.service';
+
+@Component({
+  selector: 'app-loans',
+  imports: [CommonModule],
+  templateUrl: './loans.html',
+  styleUrl: './loans.css',
+})
+
+export class Loans implements OnInit {
+
+  loans: any[] = [];
+  today = new Date();
+
+  constructor(private loansService: LoansService) {}
+
+  ngOnInit(): void {
+    this.loadLoans();
+  }
+
+  loadLoans() {
+    const user = JSON.parse(localStorage.getItem('user') || '{}');
+
+    if (!user?.id) {
+      alert('You must be logged in');
+      return;
+    }
+
+    this.loansService.getLoans(user.id)
+      .subscribe({
+        next: (data) => this.loans = data,
+        error: (err) => console.error(err)
+      });
+  }
+
+  returnBook(id: string) {
+    if (!confirm('Return this book?')) return;
+
+    this.loansService.returnLoan(id).subscribe({
+      next: () => {
+        this.loadLoans();
+      },
+      error: err => alert(err.error?.message || 'Return failed')
+    });
+  }
+
+  isOverdue(loan: any): boolean {
+    if (loan.status !== 'borrowed') return false;
+
+    const returnDate = new Date(loan.return_date);
+    return returnDate < this.today;
+  }
+}
